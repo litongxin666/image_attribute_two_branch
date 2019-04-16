210460bb8236d5e037bbb0a313ad462b99f456c3
litongxin666 <13840942791@163.com>
2019-04-16 22:24:51 +0800

    change run from local

--- run_two_branch.sh
+++ run_two_branch.sh
@@ -1,17 +1,17 @@
 if [ "$1" = "--train" ]; then
     CUDA_VISIBLE_DEVICES=0 \
-    python train_embedding_nn.py \
-    --image_feat_path /path/to/image_feature_train.mat \
-    --sent_feat_path /path/to/text_feature_train.mat \
-    --save_dir /path/to/save_dir/two-branch-ckpt
-fi
-
-if [ "$1" = "--test" ]; then
-    CUDA_VISIBLE_DEVICES=0 \
     python3.5 train_embedding_nn.py \
     --image_feat_path /home/litongxin/image_attribute_two_branch/img_feat.mat \
     --sent_feat_path /home/litongxin/Two_branch_network/two_branch_img_feature.mat \
     --save_dir /home/litongxin/image_attribute_two_branch
+fi
+
+if [ "$1" = "--test" ]; then
+    CUDA_VISIBLE_DEVICES=5 \
+    python3.5 eval_embedding_nn.py \
+    --image_feat_path /home/litongxin/image_attribute_two_branch/img_feat_test.mat \
+    --sent_feat_path /home/litongxin/Two_branch_network/two_branch_img_feature.mat \
+    --restore_path /home/litongxin/image_attribute_two_branch-876.meta
 fi
 
 if [ "$1" = "--val" ]; then
